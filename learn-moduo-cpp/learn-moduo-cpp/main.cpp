a3681fb649eb8b57f350db8dbd27415c65a53bec
guxuan <guxuan@gudeMacBook-Pro.local>
2019-12-24 22:51:48 +0800

    init project

---
+++ learn-moduo-cpp/learn-moduo-cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  learn-moduo-cpp
+//
+//  Created by guxuan on 2019/12/24.
+//  Copyright © 2019 guxuan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
